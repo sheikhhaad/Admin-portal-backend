564ff5b9643c07777f92b6d706e026f660a96338
sheikhhaad <sheikhhaad6@gmail.com>
2025-10-29 15:17:32 +0500

    logout with token blacklist

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -5,12 +5,19 @@ import { TokenModel } from "../models/tokenModel.js";
 
 dotenv.config();
 
-export const verifyToken = (req, res, next) => {
+export const verifyToken = async (req, res, next) => {
   const token = req.headers.authorization?.split(" ")[1];
 
-  if (!token) return res.status(401).json({ message: "Access denied. No token provided." });
+  if (!token)
+    return res
+      .status(401)
+      .json({ message: "Access denied. No token provided." });
 
   try {
+    // const blacklisted = await TokenModel.isTokenBlacklisted(token);
+    // if (blacklisted)
+    //   return res.status(401).json({ message: "Token invalid (user logged out)" });
+
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     next();
@@ -22,6 +29,8 @@ export const verifyToken = (req, res, next) => {
 // Role-based check
 export const verifyRole = (role) => (req, res, next) => {
   if (req.user.role !== role)
-    return res.status(403).json({ message: "Access denied. Insufficient role." });
+    return res
+      .status(403)
+      .json({ message: "Access denied. Insufficient role." });
   next();
 };
