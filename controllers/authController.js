cf3ed70b209d76042364127f34ba04be6a0867e2
sheikhhaad <sheikhhaad6@gmail.com>
2025-11-04 20:04:53 +0500

    cookies added

--- controllers/authController.js
+++ controllers/authController.js
@@ -58,6 +58,13 @@ export const loginUser = async (req, res) => {
       { expiresIn: "1d" }
     );
 
+    res.cookie("token", token, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite: "lax",
+      maxAge: 1 * 24 * 60 * 60 * 1000, // 1 days
+    });
+
     res.status(200).json({
       success: true,
       message: "Login successful",
