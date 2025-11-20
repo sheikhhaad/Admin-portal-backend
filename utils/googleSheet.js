e29acd6a09fc325f9af79fe0a4823f6564b516c8
sheikhhaad <sheikhhaad6@gmail.com>
2025-11-21 01:16:20 +0500

    done

--- utils/googleSheet.js
+++ utils/googleSheet.js
@@ -1,17 +1,19 @@
-import { google } from "googleapis";
-import dotenv from "dotenv";
-import fs from "fs";
+import { google } from "googleapis"
+import dotenv from "dotenv"
+import fs from "fs"
 
-dotenv.config();
+dotenv.config()
 
 export const appendToSheet = async (values) => {
   try {
-    const auth = new google.auth.GoogleAuth({
-      credentials: JSON.parse(fs.readFileSync("./google-service-account-sheet.json")),
-      scopes: ["https://www.googleapis.com/auth/spreadsheets"],
-    });
+    const creds = fs.readFileSync("./google-service-account-sheet.json", "utf8")
 
-    const sheets = google.sheets({ version: "v4", auth });
+    const auth = new google.auth.GoogleAuth({
+      credentials: JSON.parse(creds),
+      scopes: ["https://www.googleapis.com/auth/spreadsheets"]
+    })
+
+    const sheets = google.sheets({ version: "v4", auth })
 
     const response = await sheets.spreadsheets.values.append({
       spreadsheetId: process.env.GOOGLE_SHEET_ID,
@@ -19,12 +21,12 @@ export const appendToSheet = async (values) => {
       valueInputOption: "RAW",
       insertDataOption: "INSERT_ROWS",
       requestBody: {
-        values: [values],
-      },
-    });
+        values: [values]
+      }
+    })
 
-    console.log("Sheet updated:", response.data);
+    console.log("Sheet updated:", response.data)
   } catch (err) {
-    console.error("Google Sheet Error:", err);
+    console.error("Google Sheet Error:", err)
   }
-};
+}
